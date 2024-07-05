3d3d67a30672cb711ba343f18c4077bbc6416740
Fh-Shadow <flaviohps8@gmail.com>
2024-07-05 01:02:22 -0300

    update project

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import babel from '@rollup/plugin-babel'
-import { terser } from 'rollup-plugin-terser'
-import globImport from 'rollup-plugin-glob-import'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import globImport from 'rollup-plugin-glob-import';
 
 export default {
   input: 'src/index.js',
@@ -25,4 +25,4 @@ export default {
     terser(),
     globImport()
   ],
-}
+};
